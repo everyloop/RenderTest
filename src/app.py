a588500c6192e68f7f16429dba2df403149083b7
Fredrik Johansson <fredrik@everyloop.com>
2023-11-16 19:33:48 +0100

    Stock market app

--- src/app.py
+++ src/app.py
@@ -1,26 +1,36 @@
-import seaborn as sns
-from dash import Dash, html, dash_table, dcc, callback, Output, Input
-import plotly_express as px
+from dash import Dash, html, dcc, callback, Input, Output, State
+import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
+import pandas as pd
 
-tips = sns.load_dataset("tips")
+load_figure_template("cyborg")
 
-app = Dash(__name__)
-server = app.server
+df = pd.read_csv("../data/stooq.csv")
 
-app.layout = html.Div([
-    html.Div(children='Hello World! What\'s up?'),
-    html.Hr(),
-    dcc.RadioItems(options=["sex", "smoker", "time", "day", "size"], value="time", id="my-radio-buttons"),
-    dash_table.DataTable(data=tips.head().to_dict('records')),
-    dcc.Graph(figure={}, id="my-graph")
-])
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
+           meta_tags=[{'name': 'viewport',
+                       'content': 'width=device_width, intial-scale=1.0'}]
+           )
 
-@callback(
-    Output(component_id="my-graph", component_property="figure"),
-    Input(component_id="my-radio-buttons", component_property="value")
-)
-def update_graph(color_attribute):
-    return px.scatter(tips, x="total_bill", y="tip", color=color_attribute, width=500, template="simple_white")
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(html.H1("Stock Market Dashboard",
+                        className='text-center text-primary mb-4 mt-3'),
+                width=6)
+    ], justify="center"),
+
+    dbc.Row([
+        dbc.Col([dcc.Dropdown(id='my_dpdn', multi=False, value="MSFT",
+                             options=[{'label': x, 'value': x} for x in df["Symbols"].unique()]),
+                dcc.Graph(id='line-fig', figure={}, className='mt-2')
+                ], width={'size': 4, 'offset': 0, 'order': 1}),
+                
+        dbc.Col([dcc.Dropdown(id='my_dpdn2', multi=True, value=["MSFT", "AAPL"],
+                             options=[{'label': x, 'value': x} for x in df["Symbols"].unique()]),
+                dcc.Graph(id='line-fig2', figure={})
+                ], width={'size': 4, 'offset': 0, 'order': 2})
+    ], justify='evenly')
+], fluid=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)#, jupyter_mode='external')
